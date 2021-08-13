514da877d7ee238789bd566b84f0da8e45faefd5
Ajay Ganapathy <lets.talk@designbyajay.com>
2021-08-13 14:42:45 -0400

    chore: rename `Vue 3 Snippets.novaextension`
    `Vue 3 Snippets.novaextension` → `Vue 3 Clips.novaextension`

--- packages/vue3/component-base/src/use/Seamlss/DOMEventListeners/index.ts
+++ packages/vue3/component-base/src/use/Seamlss/DOMEventListeners/index.ts
@@ -7,4 +7,11 @@ export * from './ScrollListeners';
 export * from './TouchListeners';
 export * from './WindowListeners';
 
-// todo: recategorize according to https://developer.mozilla.org/en-US/docs/Web/Events ?
+// todo: recategorize according to https://developer.mozilla.org/en-US/docs/Web/API/Event#introduction (see all the interfaces that extend the event interface)?
+
+// todo: MUST include following events (because vetur lists them out):
+// abort, blur, canplay, canplaythrough, change, click, contextmenu, dblclick, drag, dragend, dragenter, dragleave, dragover, dragstart, drop, durationchange, emptied, ended,error, focus, formchange, forminput, input, invalid, keydown, keypress, keyup, load, loadeddata, loadedmetadata, loadstart, mousedown, mousenter, mouseleave, mousemove, mouseout, mouseover, mouseup, mousewheel, pause, play, playing, progress, ratechange, readystatechange, reset, resize, scroll, seeked, seeking, select, show, stalled, submit, suspend, timeupdate, volumechange, waiting
+
+// see: https://github.com/microsoft/TypeScript/blob/afe9cf5307e3e34c86c3bc6d3a5be5f9033be528/lib/lib.dom.d.ts#L5550
+
+// todo: make sure you listen for modifiers. see: https://github.com/vuejs/vue-next/blob/bc7dd93f9223e8c5809ad7b95fcf8b2414181b91/packages/runtime-dom/src/directives/vOn.ts#L14
