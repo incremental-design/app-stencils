f32eddb4eaf9e8d463803fce3c8272aa0c23e896
Ajay Ganapathy <lets.talk@designbyajay.com>
2021-12-08 11:08:16 -0500

    docs(component-tabs): add tabs component to storybook

---
+++ packages/storybook/src/stories/TabsComponent.stories.ts
@@ -0,0 +1,10 @@
+import TabsComponent from '@incremental.design/component-tabs';
+
+export default {
+  title: 'Tabs',
+};
+
+export const MyStory = () => ({
+  components: { TabsComponent },
+  template: `<tabs-component></tabs-component>`,
+});
