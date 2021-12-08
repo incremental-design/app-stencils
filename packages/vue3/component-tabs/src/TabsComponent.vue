311077efc9081a34ac4c635be8dbd84fd850df3a
Ajay Ganapathy <lets.talk@designbyajay.com>
2021-12-08 11:07:45 -0500

    chore(component-tabs): re-stub package
    typescript completions and package definitions weren't working before

--- packages/vue3/component-tabs/src/TabsComponent.vue
+++ packages/vue3/component-tabs/src/TabsComponent.vue
@@ -4,18 +4,10 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: 'App',
   components: {},
 });
 </script>
 
 <style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
+ 
