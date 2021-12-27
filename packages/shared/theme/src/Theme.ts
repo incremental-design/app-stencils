1e58ee4de0bef2680cf7f95d61570d7cd006c7f9
Ajay Ganapathy <ajay@incremental.design>
2021-12-26 19:42:08 -0500

    feat(theme): add `Elevation`, `Font`, `Shape` types and helper functions

--- packages/shared/theme/src/Theme.ts
+++ packages/shared/theme/src/Theme.ts
@@ -14,8 +14,7 @@
  *
  * * This class furnishes a tree of nested static methods. To use it:
  *  1. Call Theme.{@link platform} with one of {@link Theme.platforms}, or your own options object.
- *  2. Chain a call to {@link layout} with one of {@link Theme.layouts}, or your own options object.
- *  3. Chain a call to either {@link text} or {@link fill}. Each will return an object containing the available methods for the given platform and layout.
+ *  2. Chain a call to {@link layout} with one of {@link Theme.layouts}, or your own options object. It will return an object that contains a 'text', 'fill.foreground', and 'fill.background' entry. Eahc entry is an object containing the available {@link StyleGenerator}s for the given platform and layout.
  *  4. Destructure the method you want to call, and then supply it with the {@link theme.states}, {@link Tint} and `true` or `false` for the `dark?` parameter. It will return an object that contains the style.
  *
  * @example
@@ -220,6 +219,9 @@ class Layout {
 
   // !Private (and/or readonly) Properties
   #name: string;
+  #textStyleGenerators: Map<string, StyleGenerator> = new Map();
+  #foregroundStyleGenerators: Map<string, StyleGenerator> = new Map();
+  #backgroundStyleGenerators: Map<string, StyleGenerator> = new Map();
 
   // !Constructor Function
   constructor(nameOfLayout: string) {
@@ -231,7 +233,28 @@ class Layout {
     return this.#name;
   }
 
+  get text(): { [textStyleName: string]: StyleGenerator } {
+    return Object.fromEntries(this.#textStyleGenerators);
+  }
+
+  get fill(): {
+    foreground: { [fillName: string]: StyleGenerator };
+    background: { [fillName: string]: StyleGenerator };
+  } {
+    return {
+      foreground: Object.fromEntries(this.#foregroundStyleGenerators),
+      background: Object.fromEntries(this.#backgroundStyleGenerators),
+    };
+  }
+
   // !Public Instance Methods
+  addText(): void {
+    throw new Error('not implemented');
+  }
+
+  addFG(): void {}
+
+  addBG(): void {}
 
   // !Private Subroutines
 }
@@ -256,7 +279,7 @@ type StyleGenerator = (
   },
   tint?: string,
   dark?: boolean | string
-) => { SO: /* (S)tyle (O)bject */ object };
+) => { SO: /* (S)tyle (O)bject */ { [cssRule: string]: string } };
 
 /**
  * ColorPalette contains ALL of the colors used in ALL of the layouts in a given {@link Platform}'s styles.
@@ -285,13 +308,13 @@ type ColorPalette<T> = {
     };
   };
   text: {
-    title: Tint<T>;
-    headline: Tint<T>;
-    subhead: Tint<T>;
-    icon: Tint<T>;
-    body: Tint<T>;
-    footnote: Tint<T> | undefined;
-    [typeTreatmentName: string]: Tint<T> | undefined;
+    title: Tint<Font<T>>;
+    headline: Tint<Font<T>>;
+    subhead: Tint<Font<T>>;
+    icon: Tint<Font<T>>;
+    body: Tint<Font<T>>;
+    footnote: Tint<Font<T>> | undefined;
+    [typeTreatmentName: string]: Tint<Font<T>> | undefined;
   };
 };
 
@@ -326,6 +349,7 @@ type Tint<T> = {
 };
 
 /**
+ * Color, defined with a red, green, blue, and alpha value.
  * @param r - number between 0 and 255
  * @param g - number between 0 and 255
  * @param b - number between 0 and 255
@@ -339,41 +363,288 @@ type RGBA = {
 };
 
 /**
- * @param fill - the fill color
+ * A set of fills, inner shadows, strokes, and drop shadows to apply to a DOM node.
  *
- * @param stroke.color  - the stroke color
+ * @param fill - An array of zero or more fills to apply to a DOM node.
  *
- * @param stroke.width - the width of the stroke in pixels
+ * @param fill.color - a color to apply to the fill. This color must contain a `r`, `g`, `b`, and `a` property. This property is required.
  *
- * @param stroke.offset - whether the stroke is on the inside, center or outside of the shape
+ * @param fill.blendMode - optional {@link BlendMode} to apply to the fill. Defaults to {@link BlendMode.normal}
  *
- * @param stroke.borderRadius - the radius of the corners of the stroke in pixels
+ * @param bgBlur - optional gaussian blur applied to the contents behind the fill.
  *
- * @param shadow - an array of one or more box shadows.
+ * @param bgBlur.radius - optional radius of the background blur in device-independent points (pt). defaults to 0.
  *
- * @param shadow.x - the x offset of the shadow in pixels
+ * @param bgBlur.saturation - optional saturation of the background blur. defaults to 0. Must be a number between -1 (i.e. -100%) and 1 (i.e. 100%).
  *
- * @param shadow.y - the y offset of the shadow in pixels
+ * @param innerShadow - array with zero or more inner shadows to apply to a DOM node.
  *
- * @param shadow.blur - the blur of the shadow in pixels
+ * @param innerShadow.blur - optional blur of the inner shadow in device-independent points (pt). defaults to 0.
  *
- * @param shadow.spread - the spread of the shadow in pixels
+ * @param innerShadow.spread - optional spread of the inner shadow in device-independent points (pt). defaults to 0.
  *
- * @param shadow.color - the color of the shadow
+ * @param innerShadow.color - a color to apply to the inner shadow. This color must contain a `r`, `g`, `b`, and `a` property. This property is required.
+ *
+ * @param innerShadow.blendMode - optional {@link BlendMode} to apply to the inner shadow. Defaults to {@link BlendMode.normal}.
+ *
+ * @param innerShadow.xOffset - optional x-offset of the inner shadow in device-independent points (pt). defaults to 0.
+ *
+ * @param innerShadow.yOffset - optional y-offset of the inner shadow in device-independent points (pt). defaults to 0.
+ *
+ * @param stroke - an array with zero or more strokes to apply to a DOM node.
+ *
+ * @param stroke.color - a color to apply to the stroke. This color must contain a `r`, `g`, `b`, and `a` property. This property is required.
+ *
+ * @param stroke.width - optional width of the stroke in device-independent points (pt). defaults to 1.
+ *
+ * @param stroke.offset - optional offset of the stroke relative to the edge of the DOM node. can be one of 'inside', 'outside', or 'center'. defaults to 'center'.
+ *
+ * @param stroke.blendMode - optional {@link BlendMode} to apply to the stroke. Defaults to {@link BlendMode.normal}.
+ *
+ * @param dropShadow - array of zero or more drop shadows to apply to the DOM node.
+ *
+ * @param dropShadow.x - optional x-offset of the drop shadow in device-independent points (pt). defaults to 0.
+ *
+ * @param dropShadow.y - optional y-offset of the drop shadow in device-independent points (pt). defaults to 0.
+ *
+ * @param dropShadow.blur - optional blur of the drop shadow in device-independent points (pt). defaults to 0.
+ *
+ * @param dropShadow.spread - optional spread of the drop shadow in device-independent points (pt). defaults to 0.
+ *
+ * @param dropShadow.color - a color to apply to the drop shadow. This color must contain a `r`, `g`, `b`, and `a` property. This property is required.
+ *
+ * @param dropShadow.blendMode - optional {@link BlendMode} to apply to the drop shadow. Defaults to {@link BlendMode.normal}.
+ *
+ * @remarks
+ *
+ * The predefined {@link Theme.platforms} only ever use at most ONE fill, ONE stroke, and ONE inner shadow to define any given elevation. This is because CSS doesn't support multiple fills or strokes. Although CSS supports multiple inner shadows, it's usually unncessary to use more than one to mimic the appearance of elements in any of the {@link Theme.platforms} visual languages.
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/CSS/background-color
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/CSS/border
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/CSS/box-shadow
+ *
  */
 type Elevation<T> = {
-  fill: T /* yes, I now that this doesn't support gradients ... but it doesn't have to for now. Also, this is a generic type, so <T> can be adjusted to be a gradient */;
-  stroke: {
+  fill: Array<{
     color: T;
-    width: number;
-    offset: 'inner' | 'outer' | 'center';
-    borderRadius: number;
-  };
-  shadow: Array<{
-    x: number;
-    y: number;
-    blur: number;
-    spread: number;
+    blendMode?: BlendMode /* default to BlendMode.normal */;
+    bgBlur?: {
+      radius?: number /* in pt, defaults to 0pt */;
+      saturation?: number /* -1 to 1, defaults to 0 */;
+    };
+  }>;
+  innerShadow: Array<{
+    blur?: number /* in pt, defaults to 0pt */;
+    spread?: number /* in pt, defaults to 0pt */;
     color: T;
+    blendMode?: BlendMode /* defaults to BlendMode.normal */;
+    xOffset?: number /* in pt, defaults to 0pt */;
+    yOffset?: number /* in pt, defaults to 0pt */;
+  }>;
+  stroke: Array<{
+    color: T;
+    width?: number /* in pt, defaults to 1pt */;
+    offset?: 'inner' | 'outer' | 'center' /* defaults to 'center' */;
+    blendMode?: BlendMode /* defaults to BlendMode.normal */;
+  }>;
+  dropShadow: Array<{
+    x?: number /* in pt, defaults to 0pt */;
+    y?: number /* in pt, defaults to 0pt */;
+    blur?: number /* in pt, defaults to 0pt */;
+    spread?: number /* in pt, defaults to 0pt */;
+    color: T;
+    blendMode?: BlendMode /* defaults to BlendMode.normal */;
   }>;
 };
+
+function makeElevationCSSRules(E: Elevation<RGBA>) {
+  const { fill, innerShadow, stroke, dropShadow } = E;
+
+  const rules = {};
+
+  /* notice that right now we are literally discarding any fills other than fill[0] ... in fact the fill array is literally only included for future-proofing. Perhaps at some point we will use pseudo elements or maybe box shadows to hold the extra fills */
+  if (fill[0])
+    Object.assign(rules, {
+      'background-color': RGBAtoCSS(fill[0].color),
+      'background-blend-mode': fill[0].blendMode || BlendMode.normal,
+    });
+
+  if (fill[0].bgBlur)
+    Object.assign(rules, {
+      'backdrop-filter': `blur(${fill[0].bgBlur.radius || 0}pt) saturate(${
+        (fill[0].bgBlur.saturation || 0) * 100
+      }%)`,
+    });
+
+  /* notice that we are discarding everything other than the first stroke for the same reason as we are discarding any fills other than the first fill */
+  if (stroke[0])
+    Object.assign(rules, {
+      'border-color': RGBAtoCSS(stroke[0].color),
+      'border-width': `${stroke[0].width || 1}pt`,
+      'border-style': 'solid',
+      /* right now we discard offset and blend mode ... later on we can test using box shadow to achieve the same result */
+    });
+
+  const BoxShadows = [];
+  /* notice that right now we are only using the first inner shadow value. This isn't because of any intrinsic limitation in css box-shadow. It's just because we don't need to supply multiple inner shadows (for now) */
+  if (innerShadow[0]) {
+    const { xOffset, yOffset, blur, spread, color, blendMode } = innerShadow[0];
+    const x = xOffset || 0;
+    const y = yOffset || 0;
+    const b = blur || 0;
+    const s = spread || 0;
+    const c = RGBAtoCSS(color);
+    // const bm = blendMode || BlendMode.normal; /* for right now, no blend mode support */
+    BoxShadows.push(`inset ${x}pt ${y}pt ${b}pt ${s}pt ${c}`);
+  }
+
+  dropShadow.forEach(({ x, y, blur, spread, color, blendMode }) => {
+    const xOffset = x || 0;
+    const yOffset = y || 0;
+    const blurOffset = blur || 0;
+    const spreadOffset = spread || 0;
+    const c = RGBAtoCSS(color);
+    // const bm = blendMode || BlendMode.normal; /* for right now, no blend mode support */
+    BoxShadows.push(
+      `${xOffset}pt ${yOffset}pt ${blurOffset}pt ${spreadOffset}px ${c}`
+    );
+  });
+
+  if (BoxShadows.length > 0)
+    Object.assign(rules, { 'box-shadow': BoxShadows.join(', ') });
+
+  return rules;
+}
+
+enum BlendMode {
+  normal = 'normal',
+  multiply = 'multiply',
+  screen = 'screen',
+  overlay = 'overlay',
+  darken = 'darken',
+  lighten = 'lighten',
+  colorDodge = 'color-dodge',
+  colorBurn = 'color-burn',
+  hardLight = 'hard-light',
+  softLight = 'soft-light',
+  difference = 'difference',
+  exclusion = 'exclusion',
+  hue = 'hue',
+  saturation = 'saturation',
+  color = 'color',
+  luminosity = 'luminosity',
+}
+
+/**
+ * A font to apply to a DOM node's text.
+ *
+ * @param typeface - the name of the typeface to apply (e.g. 'Helvetica'). This is required.
+ *
+ * @param size - the size of the font in device-independent points (pt). This is required.
+ *
+ * @param weight - a number between 100 and 900, inclusive, where 100 is the lightest and 900 is the heaviest. This is required.
+ *
+ * @param color - a color to apply to the font, which must contain a `r`, `g`, `b`, and `a` property. This is required.
+ *
+ * @param blendMode - optional {@link BlendMode} to apply to the font. Defaults to {@link BlendMode.normal}.
+ *
+ * @param tracking - optional tracking of the font in device-independent points (pt). defaults to 0. Corresponds to {@link https://developer.mozilla.org/en-US/docs/Web/CSS/letter-spacing}
+ *
+ * @param leading - optional leading of the font in device-independent points (pt). defaults to 0. Corresponds to {@link https://developer.mozilla.org/en-US/docs/Web/CSS/line-height}
+ *
+ * @param align - optional alignment of the font. Defaults to 'left'. Corresponds to {@link https://developer.mozilla.org/en-US/docs/Web/CSS/text-align}
+ *
+ * @param verticalAlign - optional vertical alignment of the font within a flex container. Defaults to 'top'. Corresponds to {@link https://developer.mozilla.org/en-US/docs/Web/CSS/align-self}
+ *
+ * @remarks
+ *
+ * If you want your fonts to use em instead of pt, you need to apply your own font-size to the DOM node.
+ *
+ */
+type Font<T> = {
+  typeface: string;
+  size: number;
+  weight: number;
+  color: T;
+  blendMode?: BlendMode /* defaults to BlendMode.normal */;
+  tracking?: number /* in pt, omitted if not specified */;
+  leading?: number /* in pt, also known as line height */;
+  align?: 'left' | 'right' | 'center' | 'justify' /* defaults to 'left' */;
+  verticalAlign?: 'top' | 'bottom' | 'middle' /* defaults to 'top' */;
+};
+
+function makeFontCSSRules(F: Font<RGBA>) {
+  const {
+    typeface,
+    size,
+    weight,
+    color,
+    blendMode,
+    tracking,
+    leading,
+    align,
+    verticalAlign,
+  } = F;
+
+  const rules = {
+    fontFamily: typeface,
+    fontSize: `${size}pt`,
+    fontWeight: weight,
+    color: RGBAtoCSS(color),
+    'mix-blend-mode': blendMode ? blendMode : 'normal',
+  };
+  if (tracking) Object.assign(rules, { 'letter-spacing': `${tracking}pt` });
+  if (leading) Object.assign(rules, { 'line-height': `${leading}pt` });
+  if (align) Object.assign(rules, { 'text-align': align });
+  if (verticalAlign)
+    Object.assign(rules, {
+      'align-self':
+        verticalAlign === 'top'
+          ? 'start'
+          : verticalAlign === 'bottom'
+          ? 'end'
+          : 'center',
+    });
+  return rules;
+}
+
+/**
+ * A shape to apply to a DOM node
+ *
+ * @param minWidth - the minimum width of the shape in device-independent points (pt) This must be a value greater than 0. this is required.
+ *
+ * @param maxWidth - the optional maximum width of the shape in device-independent points (pt). Defaults to the minWidth.
+ *
+ * @param minHeight - the minimum height of the shape in device-independent points (pt) This must be a value greater than 0. this is required.
+ *
+ * @param maxHeight - the optional maximum height of the shape in device-independent points (pt). Defaults to the minHeight.
+ *
+ * @param borderRadius - the radius of the shape in device-independent points (pt) This must be a value greater than 0. This is required.
+ *
+ */
+type Shape = {
+  minWidth: number /* in pt */;
+  maxWidth?: number /* in pt */;
+  minHeight: number /* in pt */;
+  maxHeight?: number /* in pt */;
+  borderRadius: number /* in pt*/;
+};
+
+function makeShapeCSSRules(S: Shape) {
+  const { minWidth, maxWidth, minHeight, maxHeight, borderRadius } = S;
+  return {
+    display: 'flex',
+    'flex-direction': 'row',
+    'min-width': `${minWidth}pt`,
+    'max-width': `${maxWidth ? maxWidth : minWidth}pt`,
+    'min-height': `${minHeight}pt`,
+    'max-height': `${maxHeight ? maxHeight : minHeight}pt`,
+    'border-radius': `${borderRadius}pt`,
+  };
+}
+
+function RGBAtoCSS(R: RGBA) {
+  return `rgba(${R.r}, ${R.g}, ${R.b}, ${R.a})`;
+}
