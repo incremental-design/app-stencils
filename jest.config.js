e677153e4a2422835a5700a6970b7f0983ed6a3f
Ajay Ganapathy <ajay@incremental.design>
2021-12-27 13:51:49 -0500

    chore: make jest look for tests in any package folder

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
-  projects: ['packages/shared/*', 'packages/vue3/*', 'packages/storybook']
+  projects: ['packages/shared/*', 'packages/vue3/*', 'packages/storybook'],
+  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
 }
