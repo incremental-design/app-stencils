08d37cde4d137724a19eb3765b87bf465a86b8e1
Ajay Ganapathy <ajay@incremental.design>
2022-07-04 19:12:45 -0400

    fix(component-base): make an `index.ts` from which to export all package types

---
+++ packages/vue3/component-base/src/index.ts
@@ -0,0 +1,5 @@
+import BaseComponent from './BaseComponent.vue'
+import { State, StateChangePayload, PointerInputPayload } from './useEmits'
+
+export default BaseComponent
+export { State, StateChangePayload, PointerInputPayload }
