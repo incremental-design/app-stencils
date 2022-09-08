d839c6099eb210421ec4641e80657964b3b9b675
Ajay Ganapathy <ajay@incremental.design>
2022-09-08 16:53:36 -0400

    chore: remove jest
    since we will just use vitest

--- jest.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
-  projects: ['packages/shared/*', 'packages/vue3/*', 'packages/storybook'],
-  extensionsToTreatAsEsm: ['.jsx', '.ts', '.tsx', '.vue'],
-}
