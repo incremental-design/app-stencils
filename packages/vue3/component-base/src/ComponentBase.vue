132fbcdf8781f3b18a62396b5dcd41554fa50ec1
Ajay Ganapathy <lets.talk@designbyajay.com>
2022-10-01 09:21:00 -0400

    chore(component-base): refactor props

--- packages/vue3/component-base/src/ComponentBase.vue
+++ packages/vue3/component-base/src/ComponentBase.vue
@@ -53,215 +53,36 @@ import {
 import { EventInfo } from '@incremental.design/device-input-event-handlers/dist/types/event-handlers/handler-utils';
 
 import emits, { State } from './useEmits'
+import props from './useProps'
 
 interface EventHandlers {
-        notPassive: {
-          mouse: {
-            [eventType: string]:
-           ((E: MouseEvent) => void)
-          },
-          other: {
-            [eventType: string]:
-             | ((E: DragEvent) => void)
-             | ((E: Event) => void)
-             | ((E: FocusEvent) => void)
-             | ((E: KeyboardEvent) => void)
-             | ((E: WheelEvent) => void)
-          }
-        },
-        passive: {
-          touch: {
-         [eventType: string]: ((E: TouchEvent) => void)
-          },
-          wheel: {
-         [eventType: string]: ((E: WheelEvent) => void);
-          }
-        },
-      }
+  notPassive: {
+    mouse: {
+      [eventType: string]:
+      ((E: MouseEvent) => void)
+    },
+    other: {
+      [eventType: string]:
+        | ((E: DragEvent) => void)
+        | ((E: Event) => void)
+        | ((E: FocusEvent) => void)
+        | ((E: KeyboardEvent) => void)
+        | ((E: WheelEvent) => void)
+    }
+  },
+  passive: {
+    touch: {
+    [eventType: string]: ((E: TouchEvent) => void)
+    },
+    wheel: {
+    [eventType: string]: ((E: WheelEvent) => void);
+    }
+  },
+}
 
 export default defineComponent({
 
-  props: {
-    /**
-     * isHoverable - whether the component's appearance should change when a mouse cursor occludes it.
-     *
-     * @values true | false
-     *
-     * @example
-     * ```vue
-     *
-     * <template>
-     *  <base-component isHoverable>
-     *    <template v-slot:default>
-     *     <!-- ... -->
-     *    </template>
-     *  </base-component>
-     * </template>
-     *
-     * ```
-     */
-    isHoverable: {
-      type: Boolean,
-      default: false,
-    },
-
-    /**
-     * isPeekable - whether the component should increase in size and reveal its contents when it is hovered.
-     *
-     * @values true | false
-     *
-     * @example
-     * ```vue
-     *
-     * <template>
-     *  <base-component isPeekable>
-     *    <template v-slot:default>
-     *     <!-- ... -->
-     *    </template>
-     *  </base-component>
-     * </template>
-     *
-     * ```
-     */
-    isPeekable: {
-      type: Boolean,
-      default: false,
-    },
-
-    /**
-     * isPressable - whether the component's appearance should change when it is clicked or tapped.
-     *
-     * @values true | false
-     *
-     * @example
-     * ```vue
-     *
-     * <template>
-     *  <base-component isPeekable>
-     *    <template v-slot:default>
-     *     <!-- ... -->
-     *    </template>
-     *  </base-component>
-     * </template>
-     *
-     * ```
-     */
-    isPressable: {
-      type: Boolean,
-      default: false,
-    },
-
-    /**
-     * isToggleable - whether the component should maintain its appearance when it is pressed and the mouse cursor or touch point is released.
-     *
-     * @values true | false
-     *
-     * @example
-     * ```vue
-     *
-     * <template>
-     *  <base-component isToggleable>
-     *    <template v-slot:default>
-     *     <!-- ... -->
-     *    </template>
-     *  </base-component>
-     * </template>
-     *
-     * ```
-     */
-    isToggleable: {
-      type: Boolean,
-      default: false,
-    },
-
-    /**
-     * isSlideable - whether the components' contents follow (or otherwise respond to) the mouse cursor or touch point when the component is pressed.
-     *
-     * @values true | false
-     *
-     * @example
-     * ```vue
-     *
-     * <template>
-     *  <base-component isSlideable>
-     *    <template v-slot:default>
-     *     <!-- ... -->
-     *    </template>
-     *  </base-component>
-     * </template>
-     *
-     * ```
-     */
-    isSlideable: {
-      type: Boolean,
-      default: false,
-    },
-
-    /**
-     * isSelectable - whether the component's contents can be copied to the clipboard.
-     *
-     * @values true | false
-     *
-     * @example
-     * ```vue
-     *
-     * <template>
-     *  <base-component isSelectable>
-     *    <template v-slot:default>
-     *     <!-- ... -->
-     *    </template>
-     *  </base-component>
-     * </template>
-     *
-     * ```
-     */
-    isSelectable: {
-      type: Boolean,
-      default: false,
-    },
-
-    /**
-     * isFocusable - whether the component can receive keypresses. Note that this does NOT mean that the component's contents can be edited.
-     * @values true | false
-     *
-     * @example
-     * ```vue
-     *
-     * <template>
-     *  <base-component isToggleable>
-     *    <template v-slot:default>
-     *     <!-- ... -->
-     *    </template>
-     *  </base-component>
-     * </template>
-     *
-     * ```
-     */
-    isFocusable: {
-      type: Boolean,
-      default: false,
-    },
-
-    /**
-     * theme - whether the component should calculate the CSS rules needed to make the template match the visual language of iOS, macOS, tvOS, android, windows, or gtk (linux)
-     *
-     * @values 'ios', 'macos', 'tvos', 'android', 'windows', 'gtk' or a {@link PlatformInterface} object.
-     *
-     * @example
-     * ```vue
-     *
-     * <template>
-     *  <base-component theme="ios">
-     *    <template v-slot:default>
-     *     <!-- ... -->
-     *    </template>
-     *  </base-component>
-     * </template>
-     *
-     * ```
-     * @ignore
-     */
-  },
+  props,
 
   emits,
 
---
+++ packages/vue3/component-base/src/useProps.ts
@@ -0,0 +1,181 @@
+export default {
+    /**
+     * isHoverable - whether the component's appearance should change when a mouse cursor occludes it.
+     *
+     * @values true | false
+     *
+     * @example
+     * ```vue
+     *
+     * <template>
+     *  <base-component isHoverable>
+     *    <template v-slot:default>
+     *     <!-- ... -->
+     *    </template>
+     *  </base-component>
+     * </template>
+     *
+     * ```
+     */
+    isHoverable: {
+        type: Boolean,
+        default: false,
+    },
+
+    /**
+     * isPeekable - whether the component should increase in size and reveal its contents when it is hovered.
+     *
+     * @values true | false
+     *
+     * @example
+     * ```vue
+     *
+     * <template>
+     *  <base-component isPeekable>
+     *    <template v-slot:default>
+     *     <!-- ... -->
+     *    </template>
+     *  </base-component>
+     * </template>
+     *
+     * ```
+     */
+    isPeekable: {
+        type: Boolean,
+        default: false,
+    },
+
+    /**
+     * isPressable - whether the component's appearance should change when it is clicked or tapped.
+     *
+     * @values true | false
+     *
+     * @example
+     * ```vue
+     *
+     * <template>
+     *  <base-component isPeekable>
+     *    <template v-slot:default>
+     *     <!-- ... -->
+     *    </template>
+     *  </base-component>
+     * </template>
+     *
+     * ```
+     */
+    isPressable: {
+        type: Boolean,
+        default: false,
+    },
+
+    /**
+     * isToggleable - whether the component should maintain its appearance when it is pressed and the mouse cursor or touch point is released.
+     *
+     * @values true | false
+     *
+     * @example
+     * ```vue
+     *
+     * <template>
+     *  <base-component isToggleable>
+     *    <template v-slot:default>
+     *     <!-- ... -->
+     *    </template>
+     *  </base-component>
+     * </template>
+     *
+     * ```
+     */
+    isToggleable: {
+        type: Boolean,
+        default: false,
+    },
+
+    /**
+     * isSlideable - whether the components' contents follow (or otherwise respond to) the mouse cursor or touch point when the component is pressed.
+     *
+     * @values true | false
+     *
+     * @example
+     * ```vue
+     *
+     * <template>
+     *  <base-component isSlideable>
+     *    <template v-slot:default>
+     *     <!-- ... -->
+     *    </template>
+     *  </base-component>
+     * </template>
+     *
+     * ```
+     */
+    isSlideable: {
+        type: Boolean,
+        default: false,
+    },
+
+    /**
+     * isSelectable - whether the component's contents can be copied to the clipboard.
+     *
+     * @values true | false
+     *
+     * @example
+     * ```vue
+     *
+     * <template>
+     *  <base-component isSelectable>
+     *    <template v-slot:default>
+     *     <!-- ... -->
+     *    </template>
+     *  </base-component>
+     * </template>
+     *
+     * ```
+     */
+    isSelectable: {
+        type: Boolean,
+        default: false,
+    },
+
+    /**
+     * isFocusable - whether the component can receive keypresses. Note that this does NOT mean that the component's contents can be edited.
+     * @values true | false
+     *
+     * @example
+     * ```vue
+     *
+     * <template>
+     *  <base-component isToggleable>
+     *    <template v-slot:default>
+     *     <!-- ... -->
+     *    </template>
+     *  </base-component>
+     * </template>
+     *
+     * ```
+     */
+    isFocusable: {
+        type: Boolean,
+        default: false,
+    },
+
+    /**
+     * theme - whether the component should calculate the CSS rules needed to make the template match the visual language of iOS, macOS, tvOS, android, windows, or gtk (linux)
+     *
+     * @values 'ios', 'macos', 'tvos', 'android', 'windows', 'gtk' or a {@link PlatformInterface} object.
+     *
+     * @example
+     * ```vue
+     *
+     * <template>
+     *  <base-component theme="ios">
+     *    <template v-slot:default>
+     *     <!-- ... -->
+     *    </template>
+     *  </base-component>
+     * </template>
+     *
+     * ```
+     * @ignore
+     */
+}
