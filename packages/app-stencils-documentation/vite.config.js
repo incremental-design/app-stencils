e486b3f8c7edf5dc17f315a76d3c17bce48a1cb9
Ajay Ganapathy <ajay@incremental.design>
2022-10-07 13:18:27 -0400

    fix(page-grid): update `vite.config.js`

--- packages/app-stencils-documentation/vite.config.js
+++ packages/app-stencils-documentation/vite.config.js
@@ -1,25 +1,23 @@
-import { defineConfig } from 'vite'
-import vuePlugin from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vuePlugin from "@vitejs/plugin-vue";
 
 export default defineConfig(({ command, ssrBuild }) => ({
-  plugins: [
-    vuePlugin(),
-  ],
+  plugins: [vuePlugin()],
   experimental: {
     renderBuiltUrl(filename, { hostType, type, ssr }) {
-      if (ssr && type === 'asset' && hostType === 'js') {
+      if (ssr && type === "asset" && hostType === "js") {
         return {
-          runtime: `__ssr_vue_processAssetPath(${JSON.stringify(filename)})`
-        }
+          runtime: `__ssr_vue_processAssetPath(${JSON.stringify(filename)})`,
+        };
       }
-    }
+    },
   },
   build: {
-    minify: false
+    minify: false,
   },
   css: {
     modules: {
-      localsConvention: 'camelCaseOnly'
-    }
-  }
-}))
+      localsConvention: "camelCaseOnly",
+    },
+  },
+}));
