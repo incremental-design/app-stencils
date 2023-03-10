a16387b4e9777da76be9172b750f92c76135dc8d
Ajay Ganapathy <ajay@incremental.design>
2023-03-10 22:28:04 +0530

    generate api doc model

--- packages/nx-plugin/shared-new/src/executors/document/executor.ts
+++ packages/nx-plugin/shared-new/src/executors/document/executor.ts
@@ -1,12 +1,9 @@
 import * as path from 'path';
-import { DocumentExecutorSchema } from './schema';
 import { ExecutorContext } from '@nrwl/devkit';
-import {
-  Extractor,
-  IConfigFile,
-  ExtractorResult,
-  ExtractorConfig,
-} from '@microsoft/api-extractor';
+import { Extractor, ExtractorConfig } from '@microsoft/api-extractor';
+import { unlink } from 'fs/promises';
+import { glob } from 'glob';
+import { DocumentExecutorSchema } from './schema';
 
 export default async function runExecutor(
   options: DocumentExecutorSchema,
@@ -16,11 +13,13 @@ export default async function runExecutor(
     context.projectsConfigurations.projects[context.projectName].targets.build
       .options;
 
-  console.log(path.resolve(context.root, outputPath));
+  const outDir = path.resolve(context.root, outputPath);
+
+  const srcUrl = undefined;
 
   const config = ExtractorConfig.prepare({
     configObject: {
-      projectFolder: path.resolve(context.root, outputPath),
+      projectFolder: outDir,
       mainEntryPointFilePath: 'index.d.ts',
       dtsRollup: {
         enabled: true,
@@ -29,12 +28,30 @@ export default async function runExecutor(
       compiler: {
         tsconfigFilePath: path.resolve(context.root, 'tsconfig.base.json'),
       },
+      docModel: {
+        enabled: true,
+        ...(srcUrl ? { projectFolderUrl: srcUrl } : {}),
+        apiJsonFilePath: '<projectFolder>/api.json',
+      },
     },
     configObjectFullPath: undefined,
-    packageJsonFullPath: path.resolve(context.root, outputPath, 'package.json'),
+    packageJsonFullPath: path.resolve(outDir, 'package.json'),
   });
 
-  const extractorResult = Extractor.invoke(config);
+  Extractor.invoke(config);
+
+  const dtsFiles = await glob('**/*.d.ts', {
+    cwd: outDir,
+    root: './',
+  });
+
+  const toDelete = dtsFiles.filter((d) => {
+    const { dir, base } = path.parse(d);
+    if (dir === '' && base === 'index.d.ts') return false;
+    return true;
+  });
+
+  await Promise.all(toDelete.map(async (item) => await unlink(item)));
 
   return {
     success: true,
