6c55fcd92475cb2bd79a8dba0b6fdc20367e19b7
Ajay Ganapathy <ajay@incremental.design>
2023-07-17 13:13:16 -0400

    add source map to vite shared config

--- typescript/shared/vite.config.ts
+++ typescript/shared/vite.config.ts
@@ -26,6 +26,7 @@ export default defineConfig({
   // Configuration for building your library.
   // See: https://vitejs.dev/guide/build.html#library-mode
   build: {
+    sourcemap: true,
     lib: {
       // Could also be a dictionary or array of multiple entry points.
       entry: `${cwd}/src/index.ts`,
