5f3b8cc4f89a037cb5f155d48f044913f60327ef
Ajay Ganapathy <ajay@incremental.design>
2023-07-23 15:04:48 -0400

    add project json to `page-grid`

--- typescript/incremental-design/app.vue
+++ typescript/incremental-design/app.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div :class="pageGrid">
+    <!-- <NuxtWelcome /> -->
   </div>
 </template>
+
+<script setup>
+// useCssModule() // see: https://vuejs.org/api/sfc-css-features.html#css-modules
+import { pageGrid } from "@incremental.design/shared-page-grid/pageGrid.module.css";
+</script>
