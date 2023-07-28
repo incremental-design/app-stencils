5ddfeaf463240453129c1eab909fad388903dd56
Ajay Ganapathy <ajay@incremental.design>
2023-07-28 15:23:02 -0400

    set up reset css
    since otherwise we get user stylesheets that are unhelpful

--- typescript/incremental-design/nuxt.config.ts
+++ typescript/incremental-design/nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  css: ['~/assets/reset.css'], /* see: https://nuxt.com/docs/getting-started/styling#the-css-property */
   vite: {
     /* from typescript/vue3/vite.config.ts */ 
     build: {
--- typescript/shared/theme/src/index.ts
+++ typescript/shared/theme/src/index.ts
@@ -10,3 +10,10 @@ export * from "./Shape";
 // even better, the stylesheet can be made tailwind-compatible, or any other css library compatible, just by building and outputting the rules to the corresponding library's selectors
 
 // and best of all, if you really wanted to use these styles AT RUNTIME, you could do so by packaging vanilla-extract into these utils. then, you would prerender styles at compile time, re-render the changed based on state on SSR, and then toss out and re-render on client side (if needed). by default, you could disable toss out and rerender on hydrate, opting just to use the enumerated pre-rendered styles from vanilla, but if you wanted to enable dynamic theming for specific components, you could 'flip the switch' and use a composable to override the styles.
+
+// todo: see if https://pinceau.dev/configuration/tokens-config would work
+
+// also check out https://unocss.dev/guide/
+
+// refer to: https://nuxt.com/docs/getting-started/styling#third-party-libraries-and-modules
+
